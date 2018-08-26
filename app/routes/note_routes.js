4847ccb0a86b181bc915eaac411ee002beb5f978
Chase Hardin <chase.hardin@scribbletechnology.com>
2018-08-26 16:49:50 -0500

    Save new note to database

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -1,16 +1,13 @@
 module.exports = function(app, db) {
     app.post('/notes', (req, res) => {
-        // const note = { title: req.body.title, text: req.body.body };
+        const note = { title: req.body.title, body: req.body.body };
 
-        // db.collection('notes').insert(note, (err, result) => {
-        //     if(err) {
-        //         res.send({'error': 'An error has occurred'});
-        //     } else {
-        //         res.send(result.ops[0]);
-        //     }
-        // })
-
-        console.log(req.body);
-        res.send('Hello')
+        db.collection('notes').insertOne(note, (err, result) => {
+            if(err) {
+                res.send({'error': 'An error has occurred'});
+            } else {
+                res.send(result.ops[0]);
+            }
+        })
     });
 };
