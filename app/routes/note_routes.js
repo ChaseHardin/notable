b424cf70f9f9f6a6236cb39cc7d9c68d07e49c07
Chase Hardin <chase.hardin@scribbletechnology.com>
2018-08-26 16:59:03 -0500

    Add GET endpoint to code

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -1,13 +1,27 @@
+var ObjectID = require('mongodb').ObjectID;
+
 module.exports = function(app, db) {
     app.post('/notes', (req, res) => {
-        const note = { title: req.body.title, body: req.body.body };
+        const note = { 
+            title: req.body.title, 
+            body: req.body.body 
+        };
 
         db.collection('notes').insertOne(note, (err, result) => {
-            if(err) {
-                res.send({'error': 'An error has occurred'});
+            err ? res.send({'error': 'An error has occurred'}) : res.send(result.ops[0]);
+        });
+    });
+
+    app.get('/notes/:id', (req, res) => {
+        const id = req.params.id;
+        const details = {'_id': new ObjectID(id)};
+
+        db.collection('notes').findOne(details, (err, item) => {
+            if (err) {
+                res.send({'error':'An error has occurred'});
             } else {
-                res.send(result.ops[0]);
+                res.send(item);
             }
-        })
+        });
     });
 };
